22450ea1b9df5ee4c5bfaf24d054c5bced3c5660
achrefbenmehrez1 <45834286+achrefbenmehrez@users.noreply.github.com>
2023-02-21 21:46:35 +0100

    Initial commit

--- src/main/java/tn/esprit/realestate/Entities/User.java
+++ src/main/java/tn/esprit/realestate/Entities/User.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Builder
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstname;
     private String lastname;
