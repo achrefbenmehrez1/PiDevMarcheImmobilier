a8d8d234d4130982459c916c1f7547a64d186a0b
Fedi Jatlaoui <100429351+komodoff@users.noreply.github.com>
2023-02-22 02:20:19 +0100

    authenticate+register

--- src/main/java/tn/esprit/realestate/Entities/Role.java
+++ src/main/java/tn/esprit/realestate/Entities/Role.java
@@ -7,27 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@EqualsAndHashCode
-@Slf4j
-@Getter
-@Setter
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
 
-    private String name;
-    @ManyToMany(mappedBy = "roles")
-    private List<User> users = new ArrayList<>();
+public enum Role {
 
-    public Role(String name) {
-        this.name = name;
-    }
-
+    USER,
+    ADMIN
 }
--- src/main/java/tn/esprit/realestate/Entities/User.java
+++ src/main/java/tn/esprit/realestate/Entities/User.java
@@ -1,37 +1,78 @@
 package tn.esprit.realestate.Entities;
 
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
-@Entity
-@Table
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import tn.esprit.realestate.Security.Token;
+
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
-@EqualsAndHashCode
-@Slf4j
-@Getter
-@Setter
-@Builder
-public class User {
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
     private String firstname;
     private String lastname;
-    private String username;
     private String email;
     private String password;
 
-    @ManyToMany
-    private List<Role> roles;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @OneToMany(mappedBy = "user")
     private List<Token> tokens;
 
     @ManyToOne
     private Agency agency;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
